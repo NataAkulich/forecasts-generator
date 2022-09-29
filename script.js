ee2461cc3299e0ecb4e9053d5ba5203dc2a4eed5
NataAkulich <110620687+NataAkulich@users.noreply.github.com>
2022-09-29 16:11:06 +0200

    годелала работу

--- script.js
+++ script.js
@@ -1,5 +1,78 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
+
+
+const buttonClick = document.querySelector(".forecast-btn");
+
+buttonClick.addEventListener("click", clickOnMe);
+
+const element = document.querySelector("h1");
+
+const predictions = [
+
+    "Если Вы проявите инициативу,успех не заставит себя ждать",
+
+    "Ваши надежды и планы сбудутся сверх всяких ожиданий",
+
+    "Готовьтесь к романтическим приключениям",
+
+    "В этом месяце ночная жизнь для вас",
+
+    "Вам пора отдохнуть",
+
+];
+
+
+function getRandomNum(min, max) {
+
+    return Math.floor(Math.random() * (max - min)) + min;
+
+}
+
+
+function clickOnMe() {
+    const index = getRandomNum(0, predictions.length);
+    element.textContent = predictions[index];
+
+    const titleContainer = document.querySelector('.forecasts'); //Для хранения предсказаний
+
+    function createNewForecast(title, text) {
+
+        const template = document.querySelector("#forecast-item");
+
+        const newForeCast = template.content.cloneNode(true);
+
+        newForeCast.querySelector('h3').textContent = title;
+
+        newForeCast.querySelector('p').textContent = text;
+
+        titleContainer.append(newForeCast);
+
+    }
+
+    createNewForecast(predictions[index]);
+
+
+
+
+
+    const elementOne = document.querySelector(".forecast-btn");
+
+    elementOne.addEventListener("click", clickOnMe);
+
+
+    const persent = document.querySelector("p");
+    const number = (Math.floor(Math.random() * 100) + 1);
+    persent.textContent = "Вероятность:" + number + "%";
+
+
+
+
+}
+
+
+
+
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
 /* Подставляй текст нового предсказания в .current-forecast h1 */
@@ -10,6 +83,6 @@
 
 /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
 
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
+/* При генерации нового предсказания старое предсказание должно  sendInput.value «Мои предсказания» — .forecasts  */
 
-/* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+// forecast-item
